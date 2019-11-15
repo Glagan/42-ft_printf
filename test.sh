74637418fb353e4529a7c5edb7923b1b005758dc
Nicolas COLOMER <ncolomer@e2r2p4.42.fr>
2019-11-15 20:22:43 +0100

    More tests Compile tests with warnings

--- test.sh
+++ test.sh
@@ -1,9 +1,9 @@
 make
 # ft_printf test
-gcc -w -D IS_FT -L. -I. test.c -o ft_printf -lftprintf && ./ft_printf > test_ft_printf
+gcc -Wall -Wextra -Werror -D IS_FT -L. -I. test.c -o ft_printf -lftprintf && ./ft_printf > test_ft_printf
 cat -e test_ft_printf > test_ft_printf.res
 # printf test
-gcc -w test.c -o printf && ./printf > test_printf
+gcc -Wall -Wextra -Werror test.c -o printf && ./printf > test_printf
 cat -e test_printf > test_printf.res
 # diff
 if [[ $1 = "f" ]] || [[ $1 = "full" ]]; then
