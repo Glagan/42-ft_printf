67e617f9621d824f1aeb3c27116aa571fd102b9b
Nicolas COLOMER <ncolomer@e2r2p4.42.fr>
2019-11-15 21:11:54 +0100

    Separate tests with and without warnings

--- README.md
+++ README.md
@@ -8,3 +8,5 @@ No bonuses.
 
 You can run the tests with ``bash test.sh``, they will show the difference between the two results, the real ``printf`` result being on the left.  
 If you want to see the full result files you can run ``bash test.sh f``.
+
+These tests are compiled with ``-Wall -Wextra -Werror``, if you want **more** tests with warning disabled, run ``bash test_all.sh``.
--- test_all.sh
+++ test_all.sh
@@ -1,9 +1,9 @@
 make
 # ft_printf test
-gcc -Wall -Wextra -Werror -D IS_FT -L. -I. main.c -o ft_printf -lftprintf && ./ft_printf > test_ft_printf
+gcc -w -D IS_FT -L. -I. main_all.c -o ft_printf -lftprintf && ./ft_printf > test_ft_printf
 cat -e test_ft_printf > test_ft_printf.res
 # printf test
-gcc -Wall -Wextra -Werror main.c -o printf && ./printf > test_printf
+gcc -w main_all.c -o printf && ./printf > test_printf
 cat -e test_printf > test_printf.res
 # diff
 if [[ $1 = "f" ]] || [[ $1 = "full" ]]; then
